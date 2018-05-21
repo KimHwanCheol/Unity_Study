8b283d36ad906b43ba13a3b041a05d2cf8c74324
KimHwanCheol <jkgbrj@naver.com>
2018-05-21 15:09:28 +0900

    Update가 하는 역할이 무엇인지 확인

--- Sokovan/Assets/Scenes/Player.cs
+++ Sokovan/Assets/Scenes/Player.cs
@@ -9,12 +9,14 @@ public class Player : MonoBehaviour {
 
 
 	// Use this for initialization
+	//게임이 처음 시작되었을때 한번
 	void Start () {
-		playerRigidbody.AddForce(0,1000,0);
+
 	}
 	
 	// Update is called once per frame
+	//화면이 한번 깜빡일때 한번 실행
 	void Update () {
-		
+		Debug.Log("화면이 한번 깜빡임");
 	}
 }
