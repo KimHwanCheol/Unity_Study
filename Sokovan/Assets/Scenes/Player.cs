91cba805a37e9301fa68b90464b2089bda2fb202
KimHwanCheol <jkgbrj@naver.com>
2018-05-21 13:59:35 +0900

    게임이 진행될 공간 생성 및 player가 되는 '구' 생성 후 Rigidbody 적용

---
+++ Sokovan/Assets/Scenes/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	public float speed = 10f;
+	public Rigidbody playerRigidbody;
+
+
+	// Use this for initialization
+	void Start () {
+		playerRigidbody.AddForce(0,1000,0);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
