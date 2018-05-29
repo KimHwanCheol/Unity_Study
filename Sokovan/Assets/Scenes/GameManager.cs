0e1762c20f8ddabbb89b871d5a650c3564c4db0e
KimHwanCheol <jkgbrj@naver.com>
2018-05-29 14:49:33 +0900

    승리 UI 추가

--- Sokovan/Assets/Scenes/GameManager.cs
+++ Sokovan/Assets/Scenes/GameManager.cs
@@ -1,9 +1,11 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class GameManager : MonoBehaviour {
 
+	public GameObject winUI;
 	public ItemBox[] ItemBoxs;
 	// Use this for initialization
 
@@ -14,10 +16,16 @@ public class GameManager : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+
+		//getkey는 누르고 있는 동안
+		//getkeydown은 누르는 그 순간
+		if(Input.GetKeyDown(KeyCode.Space)){
+			SceneManager.LoadScene("SampleScene");
+		}
 		if(isGameOver = true){
 			return;
 		}
-		
+
 		int count=0;
 		for(int i = 0;i<ItemBoxs.Length;i++){
 			if(ItemBoxs[i].isOveraped == true){
@@ -26,6 +34,7 @@ public class GameManager : MonoBehaviour {
 		}
 		if(count == 3){
 			isGameOver = true;
+			winUI.SetActive(true);
 		}
 	}
 }
