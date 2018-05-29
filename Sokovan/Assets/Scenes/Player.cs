20b019f33b126571bfd393331309770787a4b5d4
KimHwanCheol <jkgbrj@naver.com>
2018-05-29 13:53:15 +0900

    플레이어 조작과 레벨디자인, 종료를 감지하는 오브젝트 회전까지

--- Sokovan/Assets/Scenes/Player.cs
+++ Sokovan/Assets/Scenes/Player.cs
@@ -5,18 +5,32 @@ using UnityEngine;
 public class Player : MonoBehaviour {
 
 	public float speed = 10f;
-	public Rigidbody playerRigidbody;
+	private Rigidbody playerRigidbody;
 
 
 	// Use this for initialization
 	//게임이 처음 시작되었을때 한번
 	void Start () {
-
+		playerRigidbody = GetComponent<Rigidbody>();
 	}
 	
 	// Update is called once per frame
 	//화면이 한번 깜빡일때 한번 실행
 	void Update () {
-		Debug.Log("화면이 한번 깜빡임");
+		
+		//-1 ~ +1
+		//Horizontal 키보드 수평방향에 대응되는 키가 대응되어있음.
+		float inputX = Input.GetAxis("Horizontal");
+
+		float inputZ = Input.GetAxis("Vertical");
+
+		float fallSpeed = playerRigidbody.velocity.y;
+		
+		Vector3 velocity = new Vector3(inputX,0,inputZ);
+		velocity = velocity * speed;
+
+		velocity.y = fallSpeed;
+
+		playerRigidbody.velocity = velocity;
 	}
 }
---
+++ Sokovan/Assets/Scenes/Rotator.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Rotator : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//1초에 60도를 회전하기 위해서는 Time.deltatTIME을 넣어줘야함
+		transform.Rotate(60 * Time.deltaTime ,60 * Time.deltaTime,60 * Time.deltaTime);		
+	}
+}
